08c92f07247983fce2c584f5d2036a5e328c82d6
Chauhan <achauhan1@galaxe.com>
2024-01-04 11:27:53 +0530

    change the service princpal role assignment code

--- create.sh
+++ create.sh
@@ -61,7 +61,7 @@ roleName='User Access Administrator'
 az role assignment create \
   --role "$roleName" \
   --assignee-object-id $aroClusterServicePrincipalObjectId \
-  --scope "/subscriptions/${subscriptionId}/resourceGroups/${aroResourceGroupName}"
+  --scope "subscriptions/${subscriptionId}/resourceGroups/${aroResourceGroupName}" \
   --assignee-principal-type 'ServicePrincipal' >/dev/null
 
 if [[ $? == 0 ]]; then
@@ -76,7 +76,7 @@ roleName='Contributor'
 az role assignment create \
   --role "$roleName" \
   --assignee-object-id $aroClusterServicePrincipalObjectId \
-  --resource-group $aroResourceGroupName \
+  --scope "subscriptions/${subscriptionId}/resourceGroups/${aroResourceGroupName}" \
   --assignee-principal-type 'ServicePrincipal' >/dev/null
 
 if [[ $? == 0 ]]; then
